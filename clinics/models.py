c2e05d66e2adeb35fb5226af9f8cbded7439a65a
YeJun <hayejun1013@naver.com>
2021-11-15 21:24:54 +0900

    feat: add clinic data model schema and migration files
    - 임상 정보 테이블 스키마 추가
    - 마이그레이션 파일 추가

--- clinics/models.py
+++ clinics/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from core.models import TimeStamp
+
+
+class Clinic(TimeStamp):
+    id          = models.CharField(max_length=16, primary_key=True, db_index=True)
+    name        = models.CharField(max_length=128)
+    duration    = models.CharField(max_length=16, null=True)
+    scope       = models.CharField(max_length=16, null=True)
+    type        = models.CharField(max_length=16, null=True)
+    institution = models.CharField(max_length=32, null=True)
+    trial       = models.CharField(max_length=32, null=True)
+    subjects    = models.PositiveIntegerField(null=True)
+    department  = models.CharField(max_length=32, null=True)
+    is_active   = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = "clinics"
