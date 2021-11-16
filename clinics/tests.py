3be70e5bc207c3967e102534bc9da8bd903d1a9f
dongheon yu <ptw880@gmail.com>
2021-11-16 12:02:44 +0900

    feat: create clinic list view, test code
    - clinic views.py 작성
    - clinic tests.py 작성
    - 리베이스 컴플릭트 해결

--- clinics/tests.py
+++ clinics/tests.py
@@ -48,3 +48,57 @@ class ClinicDetailViewTest(TestCase):
 
         self.assertEqual(response.status_code, 404)
         self.assertEqual(response.json(), {"message": "CLINIC_NOT_FOUND"})
+
+
+class ClinicViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.maxDiff = None
+        
+        Clinic.objects.create(
+            id  = 1,
+            name="연구_1",
+            duration="3년",
+            scope="국내다기관",
+            type="관찰연구",
+            institution="여의도성모병원",
+            trial="코호트",
+            subjects=300,
+            department="진료과"
+            )
+
+    def tearDown(self):
+        Clinic.objects.all().delete()
+        
+    def test_get_clinic_list_success(self):
+        
+        result = {
+            "current_count" : 1,
+            "data" : [
+                {
+                    "name" : "연구_1",
+                    "id" : "1",
+                    "duration" : "3년",
+                    "scope" : "국내다기관",
+                    "type" : "관찰연구",
+                    "institution" : "여의도성모병원",
+                    "trial" : "코호트",
+                    "subjects" : 300,
+                    "department" : "진료과"
+                    } 
+                ],
+            "match_count" : 1,
+            "page" : 1,
+            "per_page" : 1,
+            "total_count" : 1
+            }
+        
+        response = self.client.get("/clinics/list?page=1")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"result" : result})
+        
+    def test_get_clinic_list_wrong_page_number(self):
+        
+        response = self.client.get("/clinics/list?page=")
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {"result" : "WRONG_PAGE_NUMBER"})
--- clinics/views.py
+++ clinics/views.py
@@ -24,3 +24,40 @@ class ClinicDetailView(View):
 
         except Clinic.DoesNotExist:
             return JsonResponse({"message": "CLINIC_NOT_FOUND"}, status=404)
+
+
+class ClinicListView(View):
+    def get(self, request):
+        try:
+            page_num = int(request.GET.get('page', 1))
+            limit    = 5
+            start    = (page_num-1) * limit
+            end      = page_num     * limit
+            
+            clinics = Clinic.objects.all()[start:end]
+            
+            result = {
+                "current_count" : len(clinics),
+                "data" : [
+                    {
+                        "name" : clinic.name,
+                        "id" : clinic.id,
+                        "duration" : clinic.duration,
+                        "scope" : clinic.scope,
+                        "type" : clinic.type,
+                        "institution" : clinic.institution,
+                        "trial" : clinic.trial,
+                        "subjects" : clinic.subjects,
+                        "department" : clinic.department
+                    } for clinic in clinics
+                ],
+                "match_count" : len(Clinic.objects.all()),
+                "page" : page_num,
+                "per_page" : len(clinics),
+                "total_count" : len(Clinic.objects.all())
+            }
+            
+            return JsonResponse({"result" : result}, status=200)
+        
+        except ValueError:
+            return JsonResponse({"result" : "WRONG_PAGE_NUMBER"}, status=400)
