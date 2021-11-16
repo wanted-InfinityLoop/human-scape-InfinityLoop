95123e729380f06841c96222f9f6bb85fd606ac0
YeJun <hayejun1013@naver.com>
2021-11-17 00:13:50 +0900

    refactor: refactor project codes and align assignment operator

--- clinics/tests.py
+++ clinics/tests.py
@@ -43,15 +43,16 @@ class ClinicDetailViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {
             "data" :{
-                "name": clinic.name,
-                "id": clinic.id,
-                "duration": clinic.duration,
-                "scope": clinic.scope,
-                "type": clinic.type,
+                "name"       : clinic.name,
+                "id"         : clinic.id,
+                "duration"   : clinic.duration,
+                "scope"      : clinic.scope,
+                "type"       : clinic.type,
                 "institution": clinic.institution,
-                "trial": clinic.trial,
-                "subjects": clinic.subjects,
-                "department": clinic.department
+                "trial"      : clinic.trial,
+                "subjects"   : clinic.subjects,
+                "department" : clinic.department,
+                "updated_at" : clinic.updated_at.strftime("%Y-%m-%d %H:%M:%S")
                 }
             }
         )
@@ -66,58 +67,118 @@ class ClinicDetailViewTest(TestCase):
         response = self.client.get("/clinics/C160045")
 
         self.assertEqual(response.status_code, 404)
-        self.assertEqual(response.json(), {"message": "CLINIC_CAN'T_FOUND"})
+        self.assertEqual(response.json(), {"message": "CLINIC_NOT_FOUND"})
 
 
-class ClinicViewTest(TestCase):
+class ClinicListViewTest(TestCase):
     def setUp(self):
         self.client = Client()
         self.maxDiff = None
-        
+
         Clinic.objects.create(
-            id  = 1,
-            name="연구_1",
-            duration="3년",
-            scope="국내다기관",
-            type="관찰연구",
+            id         ="C160046",
+            name       ="연구_1",
+            duration   ="3년",
+            scope      ="국내다기관",
+            type       ="관찰연구",
             institution="여의도성모병원",
-            trial="코호트",
-            subjects=300,
-            department="진료과"
-            )
+            trial      ="코호트",
+            subjects   =300,
+            department ="진료과",
+        )
+
+        Clinic.objects.create(
+            id         ="C160047",
+            name       ="연구_1",
+            duration   ="3년",
+            scope      ="국내다기관",
+            type       ="관찰연구",
+            institution="여의도성모병원",
+            trial      ="코호트",
+            subjects   =300,
+            department ="진료과",
+            updated_at ="2021-10-12 00:00:00",
+            is_active  =False,
+        )
 
     def tearDown(self):
         Clinic.objects.all().delete()
-        
+
     def test_get_clinic_list_success(self):
-        
-        result = {
-            "current_count" : 1,
-            "data" : [
-                {
-                    "name" : "연구_1",
-                    "id" : "1",
-                    "duration" : "3년",
-                    "scope" : "국내다기관",
-                    "type" : "관찰연구",
-                    "institution" : "여의도성모병원",
-                    "trial" : "코호트",
-                    "subjects" : 300,
-                    "department" : "진료과"
-                    } 
-                ],
-            "match_count" : 1,
-            "page" : 1,
-            "per_page" : 1,
-            "total_count" : 1
-            }
-        
         response = self.client.get("/clinics/list?page=1")
+
+        clinic = Clinic.objects.get(id="C160046")
+
+        result = {
+            "data": [
+                {
+                    "name"       : clinic.name,
+                    "id"         : clinic.id,
+                    "duration"   : clinic.duration,
+                    "scope"      : clinic.scope,
+                    "type"       : clinic.type,
+                    "institution": clinic.institution,
+                    "trial"      : clinic.trial,
+                    "subjects"   : clinic.subjects,
+                    "department" : clinic.department,
+                    "updated_at" : clinic.updated_at.strftime("%Y-%m-%d %H:%M:%S"),
+                }
+            ],
+            "general_information": {
+                "count"      : 1,
+                "total_count": 2,
+                "page"       : 1,
+                "total_page" : 1,
+                "per_page"   : 10,
+            },
+        }
+
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), {"result" : result})
-        
+        self.assertEqual(response.json(), {"result": result})
+
     def test_get_clinic_list_wrong_page_number(self):
-        
         response = self.client.get("/clinics/list?page=")
+
         self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json(), {"result" : "WRONG_PAGE_NUMBER"})
+        self.assertEqual(response.json(), {"result": "WRONG_PAGE_NUMBER"})
+
+
+class ClinicListViewTest_2(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.maxDiff = None
+
+        Clinic.objects.create(
+            id         ="C160046",
+            name       ="연구_1",
+            duration   ="3년",
+            scope      ="국내다기관",
+            type       ="관찰연구",
+            institution="여의도성모병원",
+            trial      ="코호트",
+            subjects   =300,
+            department ="진료과",
+            is_active  =False,
+        )
+
+        Clinic.objects.create(
+            id         ="C160047",
+            name       ="연구_1",
+            duration   ="3년",
+            scope      ="국내다기관",
+            type       ="관찰연구",
+            institution="여의도성모병원",
+            trial      ="코호트",
+            subjects   =300,
+            department ="진료과",
+            is_active  =False,
+        )
+
+    def tearDown(self):
+        Clinic.objects.all().delete()
+
+    def test_get_clinic_list_not_matching_data(self):
+        response = self.client.get("/clinics/list?page=1")
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json(), {"result": "NOT_MATCHING_DATA"})
--- clinics/views.py
+++ clinics/views.py
@@ -1,5 +1,8 @@
-from django.views import View
-from django.http  import JsonResponse
+from datetime import datetime, timedelta
+
+from django.db.models import Q
+from django.views     import View
+from django.http      import JsonResponse
 
 from clinics.models import Clinic
 
@@ -10,18 +13,19 @@ class ClinicDetailView(View):
             clinic = Clinic.objects.get(id=clinic_id)
 
             if not clinic.is_active:
-                return JsonResponse({"message": "CLINIC_CAN'T_FOUND"}, status=404)
+                return JsonResponse({"message": "CLINIC_NOT_FOUND"}, status=404)
 
             data = {
-                "name": clinic.name,
-                "id": clinic.id,
-                "duration": clinic.duration,
-                "scope": clinic.scope,
-                "type": clinic.type,
+                "name"       : clinic.name,
+                "id"         : clinic.id,
+                "duration"   : clinic.duration,
+                "scope"      : clinic.scope,
+                "type"       : clinic.type,
                 "institution": clinic.institution,
-                "trial": clinic.trial,
-                "subjects": clinic.subjects,
-                "department": clinic.department
+                "trial"      : clinic.trial,
+                "subjects"   : clinic.subjects,
+                "department" : clinic.department,
+                "updated_at" : clinic.updated_at.strftime("%Y-%m-%d %H:%M:%S")
             }
 
             return JsonResponse({"data": data}, status=200)
@@ -33,35 +37,45 @@ class ClinicDetailView(View):
 class ClinicListView(View):
     def get(self, request):
         try:
-            page_num = int(request.GET.get('page', 1))
-            limit    = 5
-            start    = (page_num-1) * limit
-            end      = page_num     * limit
-            
-            clinics = Clinic.objects.all()[start:end]
-            
+            page_num = int(request.GET.get("page", 1))
+            limit    = 10
+            start    = (page_num - 1) * limit
+            end      = page_num * limit
+
+            q = Q(updated_at__gt=datetime.now() - timedelta(days=7))
+
+            clinics_unfiltered = Clinic.objects.all()
+            clinics_filtered   = Clinic.objects.filter(q).filter(is_active=True)[start:end]
+
+            if not clinics_filtered.exists():
+                return JsonResponse({"result": "NOT_MATCHING_DATA"}, status=404)
+
             result = {
-                "current_count" : len(clinics),
-                "data" : [
+                "data": [
                     {
-                        "name" : clinic.name,
-                        "id" : clinic.id,
-                        "duration" : clinic.duration,
-                        "scope" : clinic.scope,
-                        "type" : clinic.type,
-                        "institution" : clinic.institution,
-                        "trial" : clinic.trial,
-                        "subjects" : clinic.subjects,
-                        "department" : clinic.department
-                    } for clinic in clinics
+                        "name"       : clinic.name,
+                        "id"         : clinic.id,
+                        "duration"   : clinic.duration,
+                        "scope"      : clinic.scope,
+                        "type"       : clinic.type,
+                        "institution": clinic.institution,
+                        "trial"      : clinic.trial,
+                        "subjects"   : clinic.subjects,
+                        "department" : clinic.department,
+                        "updated_at" : clinic.updated_at.strftime("%Y-%m-%d %H:%M:%S")
+                    }
+                    for clinic in clinics_filtered
                 ],
-                "match_count" : len(Clinic.objects.all()),
-                "page" : page_num,
-                "per_page" : len(clinics),
-                "total_count" : len(Clinic.objects.all())
+                "general_information": {
+                    "count"      : clinics_filtered.count(),
+                    "total_count": clinics_unfiltered.count(),
+                    "page"       : page_num,
+                    "total_page" : clinics_unfiltered.count() // limit + 1, 
+                    "per_page"   : limit,
+                },
             }
-            
-            return JsonResponse({"result" : result}, status=200)
-        
+
+            return JsonResponse({"result": result}, status=200)
+
         except ValueError:
-            return JsonResponse({"result" : "WRONG_PAGE_NUMBER"}, status=400)
+            return JsonResponse({"result": "WRONG_PAGE_NUMBER"}, status=400)
