10ad9fe0d51fc1d9a1cceeacc1824e6817992258
Oderystella <ddott59@gmail.com>
2021-11-16 12:15:07 +0900

    feat: add get clinic detail function and test
    - 임상 정보 상세 조회 기능 추가
    - 임상 정보 상세 조회 성공 케이스
    - 임상 정보 id 없을 때 실패 케이스
    - 불필요한 코드 삭제
    - 경로 이름 변경 trials -> clinics
    - 반환값 키값 영문으로 변경

--- clinics/views.py
+++ clinics/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.views import View
+from django.http  import JsonResponse
 
-# Create your views here.
+from clinics.models import Clinic
+
+
+class ClinicDetailView(View):
+    def get(self, request, clinic_id):
+        try:
+            clinic = Clinic.objects.get(id=clinic_id)
+            data = {
+                "name": clinic.name,
+                "id": clinic.id,
+                "duration": clinic.duration,
+                "scope": clinic.scope,
+                "type": clinic.type,
+                "institution": clinic.institution,
+                "trial": clinic.trial,
+                "subjects": clinic.subjects,
+                "department": clinic.department
+            }
+
+            return JsonResponse({"data": data}, status=200)
+
+        except Clinic.DoesNotExist:
+            return JsonResponse({"message": "CLINIC_NOT_FOUND"}, status=404)
